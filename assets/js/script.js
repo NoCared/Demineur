4935b8255b215e63666291292b9068edae859ac5
nocared <paul.richarddu30@gmail.com>
2023-10-06 11:24:16 +0200

    ADD - Numbers count of bombs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,72 @@
-function setUpTable(table)
-{
-    let length = table.length;
-    while (length--)
-    {
+
+const cases = document.querySelectorAll(".case");
+const tab = [];
+
+function setUpTable() {
+    let length = cases.length;
+    while (length--) {
         const i = length;
-        table[i].addEventListener("click",()=>{
+        cases[i].addEventListener("click", () => {
             console.log(i);
-            table[i].classList.toggle("backGreen");
+            cases[i].classList.toggle("backGreen");
         });
+        tab[length] = false;
     }
 }
 
-function putRandomBombs(table)
-{
-    const bombNumber = table.length /5;
-    for (i = 0; i < bombNumber;i ++)
-    {
+function putRandomBombs() {
+    const bombNumber = cases.length / 5;
+    for (let i = 0; i < bombNumber; i++) {
         const randomNumberRow = Math.round(Math.random() * 9);
         const randomNumberColumn = Math.round(Math.random() * 9);
-        const index = randomNumberColumn *10 + randomNumberRow;
+        const index = randomNumberColumn * 10 + randomNumberRow;
 
-        if (table[index].classList.contains("bombCase") == false)
-        {
-            table[index].classList.toggle("bombCase");
+        if (checkIfBomb(index) == false) {
+            cases[index].classList.toggle("bombCase");
+            tab[index] = true
         }
-        else
-        {
+        else {
             i--;
         }
     }
 }
 
+function checkIfBomb(index) {
+    return (0 <= index && index < cases.length) ? tab[index] : false;
+}
+
+function getBombNumberNear(index) {
+    let number = 0;
+
+    for (let i = -1; i <= 1; i++) {
+
+        //Check if we do not collide on borders
+        if (Math.floor((index + i) / 10) == Math.floor(index / 10)) {
+            //Check Top line
+            number += checkIfBomb(index + i - 10) ? 1 : 0;
+            //Check Mid line
+            number += checkIfBomb(index + i) ? 1 : 0;
+            //Check Mid line
+            number += checkIfBomb(index + i + 10) ? 1 : 0;
+        }
+    }
+    return number;
+}
+
+
+
+function fillNumber() {
+    for (let i = 0; i < cases.length; i++) {
+        if (checkIfBomb(i) == false) {
+            cases[i].innerText = getBombNumberNear(i);
+        }
+    }
+}
 
 
 
 
-const cases = document.querySelectorAll(".case");
-setUpTable(cases);
-putRandomBombs(cases);
+
+setUpTable();
+putRandomBombs();
+fillNumber();
