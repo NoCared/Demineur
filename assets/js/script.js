ad3ceb13770e70e8d833f94fe529816968e4f4d8
nocared <paul.richarddu30@gmail.com>
2023-10-06 10:41:30 +0200

    ADD - Bomb génération

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,40 @@
-
-let cases = document.querySelectorAll(".case");
-let length = cases.length;
-while (length--)
+function setUpTable(table)
 {
-    const i = length;
-    cases[i].addEventListener("click",()=>{
-        console.log(i);
-        cases[i].classList.toggle("backGreen");
-    });
+    let length = table.length;
+    while (length--)
+    {
+        const i = length;
+        table[i].addEventListener("click",()=>{
+            console.log(i);
+            table[i].classList.toggle("backGreen");
+        });
+    }
 }
+
+function putRandomBombs(table)
+{
+    const bombNumber = table.length /5;
+    for (i = 0; i < bombNumber;i ++)
+    {
+        const randomNumberRow = Math.round(Math.random() * 9);
+        const randomNumberColumn = Math.round(Math.random() * 9);
+        const index = randomNumberColumn *10 + randomNumberRow;
+
+        if (table[index].classList.contains("bombCase") == false)
+        {
+            table[index].classList.toggle("bombCase");
+        }
+        else
+        {
+            i--;
+        }
+    }
+}
+
+
+
+
+
+const cases = document.querySelectorAll(".case");
+setUpTable(cases);
+putRandomBombs(cases);
