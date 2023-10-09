f4d9c746ac53ace71e924bc508ac26dca8856514
nocared <paul.richarddu30@gmail.com>
2023-10-09 09:16:56 +0200

    ADD - Discover near case when hit a 0

--- assets/js/modules/gameplay.js
+++ assets/js/modules/gameplay.js
@@ -1,32 +1,43 @@
 import { tab, cases } from "./setupGrid.js";
 
-function verify(index)
-{
+function verify(index) {
     return (0 <= index && index < tab.length) ? tab[index].number : -1;
 }
 
-function lookAt(previousNumber, index)
-{
- if (previousNumber===0 && verify(index)>0)
-{
- 
-}
+function lookAt(previousIndex, index) {
+    if (verify(index) != -1 && tab[index].isDisplayed === false) {
+        if (tab[previousIndex].number === 0 && verify(index) > 0) {
+            openCase(index);
+        }
+        else if (verify(index) === 0) {
+            openCase(index);
+            lookAround(index);
+        }
+    }
 }
 
 function lookAround(index) {
-    // let number = lookAt(index-10);
-    // if (index !== 0 && number === 0)
-    // {
-    //     discover(index-10)
-    // }
+    for (let i = -1; i <= 1; i++) {
+
+        const eulerCurrent = Math.floor(index / 10);
+        const eulerNext = Math.floor((index + i) / 10);
+        if (eulerCurrent === eulerNext) {
+            lookAt(index, index - 10 + i);
+            lookAt(index, index + i);
+            lookAt(index, index + 10 + i);
+        }
+    }
 }
 
+function openCase(index) {
+    cases[index].classList.remove("hidden");
+    tab[index].isDisplayed = true;
+}
 
 
 function discover(caseIndex) {
     if (tab[caseIndex].isDisplayed === false) {
-        cases[caseIndex].classList.remove("hidden");
-        tab[caseIndex].isDisplayed = true;
+        openCase(caseIndex);
         if (tab[caseIndex].isBomb) {
             console.log("LOSE");
         }
