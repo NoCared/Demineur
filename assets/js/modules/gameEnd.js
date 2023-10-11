3d86477d05d34edc522aba30ad3245796c4621ec
nocared <paul.richarddu30@gmail.com>
2023-10-11 16:49:53 +0200

    ADD - Win / Lose screen

--- assets/js/modules/gameEnd.js
+++ assets/js/modules/gameEnd.js
@@ -3,8 +3,15 @@
 function displayResult(isWin)
 {
     document.getElementById("overlayEnd").style.display = "block";
-
-
+    if (isWin)
+    {
+        document.getElementById("overlayWin").style.display="block";
+        document.getElementById("overlayLose").style.display="nonr";
+    }
+    else{
+        document.getElementById("overlayWin").style.display="none";
+        document.getElementById("overlayLose").style.display="block";
+    }
 }
 
 
